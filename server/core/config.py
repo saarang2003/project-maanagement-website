89e5272584042e098ba337a03a8f748362f7e6e0
sarang <saranggp2018@gmail.com>
2025-08-16 09:25:08 +0530

    first commit

---
+++ server/core/config.py
@@ -0,0 +1,15 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    mongo_uri : str
+    mongo_db : str
+    secret_key : str
+    algorithm : str
+    access_token_expire_minutes : int
+    VITE_BACKEND_APP_API_URL: str
+
+    model_config = SettingsConfigDict(env_file=".env")
+
+
+settings = Settings()
+    
