7efcf368e37463b973e47119427bca30164b2061
sarang <saranggp2018@gmail.com>
2025-08-22 23:09:11 +0530

    added types to entities

---
+++ client/src/lib/constant.ts
@@ -0,0 +1,6 @@
+const constant = {
+    ACCESS_TOKEN_KEY : "token",
+    ACCESS_TOKEN_EXPIRE :3500,
+};
+
+export default constant;
---
+++ client/src/lib/types.ts
@@ -0,0 +1,60 @@
+export interface TaskStatus {
+    _id : string;
+    title : string;
+}
+
+
+export interface User{
+    _id : String;
+    username : String;
+    email : String;
+    created_at : Date;
+    updated_at : Date;
+}
+
+export interface ErrorResponse {
+    success : boolean;
+    error : {
+        code : number;
+        message : string;
+    };
+}
+
+export interface Token{
+    access_token : string;
+    user : User;
+}
+
+export interface Task{
+    _id: string;
+    title : string;
+    description : string;
+    sort_number: string;
+    task_list_id : string;
+    due_date : Date;
+    priority : string;
+    status : string;
+    created_at : Date;
+    updated_at : Date;
+}
+
+
+export interface TaskList{
+    _id : string;
+    title : string;
+    description : string;
+    plan_id : string;
+    created_at : Date;
+    updated_at : Date;
+    tasks : Task[];
+}
+export interface Plan{
+    _id : string;
+    title : string;
+    description : string;
+    user_id : string;
+    created_at : Date;
+    updated_at : Date;
+    task_lists : TaskList[];
+}
+
